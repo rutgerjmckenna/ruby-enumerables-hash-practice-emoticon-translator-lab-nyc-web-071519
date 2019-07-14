a4a4ffa41cba7bfdfeff03bbefe9acde64a3bab9
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-14 18:14:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,15 @@
 # require modules here
+require 'pry'
+require 'yaml'
 
 def load_library
-  # code goes here
+  
 end
 
 def get_japanese_emoticon
-  # code goes here
+  
 end
 
 def get_english_meaning
-  # code goes here
+  
 end
